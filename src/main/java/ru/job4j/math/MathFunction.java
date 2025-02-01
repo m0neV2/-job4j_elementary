c87dd2f6b9ac6d9cc309bd236b3fa50edc538c6d
m0neV <sadihsalahov7@gmail.com>
2025-02-02 00:18:18 +0500

    1. Модульные тесты [#505277]

---
+++ src/main/java/ru/job4j/math/MathFunction.java
@@ -0,0 +1,16 @@
+package ru.job4j.math;
+
+public class MathFunction {
+    public static  double sum(double one, double second) {
+        return one + second;
+    }
+    public static double multiple(double one, double second) {
+        return one * second;
+    }
+    public static double minus(double one, double second) {
+        return one - second;
+    }
+    public static double delenie(double one, double second){
+        return one / second;
+    }
+}
