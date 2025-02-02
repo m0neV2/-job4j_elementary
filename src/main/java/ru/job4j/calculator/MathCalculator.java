05c2c89e25e5a39840fef8d7841bb22357a38f02
m0neV <sadihsalahov7@gmail.com>
2025-02-02 20:55:00 +0500

    1.1. Что такое import [#505278]

--- src/main/java/ru/job4j/calculator/MathCalculator.java
+++ src/main/java/ru/job4j/calculator/MathCalculator.java
@@ -1,15 +1,21 @@
 package ru.job4j.calculator;
+
 import static ru.job4j.math.MathFunction.*;
+
 public class MathCalculator {
+
     public static double sumAndMultiply(double first, double second){
         return sum(first, second) + multiple(first, second);
     }
+
     public static double minusAndDelenie(double first, double second){
         return minus(first, second) + delenie(first, second);
     }
+
     public static double all(double first, double second){
         return sum(first, second) + multiple(first, second) + minus(first, second) + delenie(first, second);
     }
+
     public static void main(String[] args){
         System.out.println("Результат рассчета равен: " + sumAndMultiply(11, 20));
         System.out.println("Результат рассчета равен: " + minusAndDelenie(11, 20));
--- src/main/java/ru/job4j/math/MathFunction.java
+++ src/main/java/ru/job4j/math/MathFunction.java
@@ -1,16 +1,21 @@
 package ru.job4j.math;
 
 public class MathFunction {
+
     public static  double sum(double first, double second) {
         return first + second;
     }
+
     public static double multiple(double first, double second) {
         return first * second;
+
     }
     public static double minus(double first, double second) {
         return first - second;
+
     }
     public static double delenie(double first, double second){
         return first / second;
+
     }
 }
