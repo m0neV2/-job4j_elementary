13fa72320dbd8eea51111781ad816f48fe321e88
m0neV <sadihsalahov7@gmail.com>
2025-02-02 21:55:35 +0500

    1.1. Что такое import [#505278 #530994]

--- src/main/java/ru/job4j/calculator/MathCalculator.java
+++ src/main/java/ru/job4j/calculator/MathCalculator.java
@@ -4,26 +4,21 @@ import static ru.job4j.math.MathFunction.*;
 
 public class MathCalculator {
 
-    public static double sumAndMultiply(double first, double second){
+    public static double sumAndMultiply(double first, double second) {
         return sum(first, second) + multiple(first, second);
-
     }
 
-    public static double minusAndDelenie(double first, double second){
+    public static double minusAndDelenie(double first, double second) {
         return minus(first, second) + delenie(first, second);
-
     }
 
-    public static double all(double first, double second){
+    public static double all(double first, double second) {
         return sum(first, second) + multiple(first, second) + minus(first, second) + delenie(first, second);
-
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Результат рассчета равен: " + sumAndMultiply(11, 20));
         System.out.println("Результат рассчета равен: " + minusAndDelenie(11, 20));
         System.out.println("Результат рассчета равен: " + all(11, 20));
-
     }
-
 }
--- src/main/java/ru/job4j/math/MathFunction.java
+++ src/main/java/ru/job4j/math/MathFunction.java
@@ -2,25 +2,19 @@ package ru.job4j.math;
 
 public class MathFunction {
 
-    public static  double sum(double first, double second) {
+    public static double sum(double first, double second) {
         return first + second;
-
     }
 
     public static double multiple(double first, double second) {
         return first * second;
-
     }
 
     public static double minus(double first, double second) {
         return first - second;
-
     }
 
-    public static double delenie(double first, double second){
+    public static double delenie(double first, double second) {
         return first / second;
-
     }
-
 }
-
