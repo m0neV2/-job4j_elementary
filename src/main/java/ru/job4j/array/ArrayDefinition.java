94ff788a118295e2cb5421288818e2fe4b1e006d
m0neV <sadihsalahov7@gmail.com>
2025-02-12 22:01:56 +0500

    Заполнение массива [#505222]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -9,5 +9,11 @@ public class ArrayDefinition {
         System.out.println(surnames.length);
         float[] prices = new float[40];
         System.out.println(prices.length);
+        String[] names = new String[4];
+        names[0] = "Sadih Salahov";
+        names[1] = "Petr Arsentev";
+        names[2] = "Korobenikov Stas";
+        names[3] = "Anton Gorlov";
+        System.out.println("Элементы массива: " + names[0] + ", " + names[1] + ", " + names[2] + ", " + names[3]);
     }
 }
