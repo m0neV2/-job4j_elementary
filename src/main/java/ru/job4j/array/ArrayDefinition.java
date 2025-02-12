a4e85ac33470b8dfd67680d380bc5be11a0568e8
m0neV <sadihsalahov7@gmail.com>
2025-02-12 21:55:03 +0500

    Размер массива [#505221]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -4,7 +4,10 @@ public class ArrayDefinition {
 
     public static void main(String[] args) {
         short[] ages = new short[10];
+        System.out.println(ages.length);
         String[] surnames = new String[100500];
+        System.out.println(surnames.length);
         float[] prices = new float[40];
+        System.out.println(prices.length);
     }
 }
