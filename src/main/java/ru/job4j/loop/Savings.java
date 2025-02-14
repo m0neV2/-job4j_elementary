2e1c7ce76ad2c0294e06dceaff7b937331567419
m0neV <sadihsalahov7@gmail.com>
2025-02-14 22:36:11 +0500

    Цикл do-while [#505216 #532115]

--- src/main/java/ru/job4j/loop/Savings.java
+++ src/main/java/ru/job4j/loop/Savings.java
@@ -1,6 +1,7 @@
 package ru.job4j.loop;
 
 public class Savings {
+
     public static int years(double goal, double annualDeposit, double percent) {
         int years = 0;
         double total = 0;
