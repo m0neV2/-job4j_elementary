a48279e43ce5b0d6c6186a802d01bfaa24d80c67
m0neV <sadihsalahov7@gmail.com>
2025-02-17 21:37:05 +0500

    Поиск индекса в диапазоне [#505237]

--- src/main/java/ru/job4j/array/FindLoop.java
+++ src/main/java/ru/job4j/array/FindLoop.java
@@ -13,6 +13,17 @@ public class FindLoop {
         return result;
     }
 
+    public static int indexInRange(int[] data, int element, int start, int finish) {
+        int result = -1;
+        for (int i = start; i <= finish; i++) {
+            if (data[i] == element) {
+                result = i;
+                break;
+            }
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
         int[] array = {2, 3, 4, 6};
         System.out.println(indexOf(array, 2));
