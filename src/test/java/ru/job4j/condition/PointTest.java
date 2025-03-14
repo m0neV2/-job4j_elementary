7c8ec96beb84dddd3775a513a06ecaf4a141fe28
m0neV <sadihsalahov7@gmail.com>
2025-03-14 21:19:54 +0500

    2.0. Рефакторинг - Расстояние между точками. [#122587]

--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -6,26 +6,29 @@ import static org.assertj.core.api.Assertions.*;
 class PointTest {
 
     @Test
-    void whenPoints5And5And8Minus8Then2() {
-        int x1 = 5, y1 = -5, x2 = 8, y2 = -8;
+    void whenPoints5AndMinus5And8AndMinus8Then4Dot24() {
+        Point point1 = new Point(5, -5);
+        Point point2 = new Point(8, -8);
         double expected = 4.24;
-        double output = Point.distance(x1, y1, x2, y2);
-        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+        double result = point1.distance(point2);
+        assertThat(result).isEqualTo(expected, withPrecision(0.01));
     }
 
     @Test
-    void whenPointsMinus5Minus7And9And12Then2Dot83() {
-        int x1 = -5, y1 = -7, x2 = 9, y2 = 12;
+    void whenPointsMinus5AndMinus7And9And12Then23Dot6() {
+        Point point1 = new Point(-5, -7);
+        Point point2 = new Point(9, 12);
         double expected = 23.6;
-        double output = Point.distance(x1, y1, x2, y2);
-        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+        double result = point1.distance(point2);
+        assertThat(result).isEqualTo(expected, withPrecision(0.01));
     }
 
     @Test
-    void whenPoints4And6And8AndMinus4Then5Dot66() {
-        int x1 = -4, y1 = 6, x2 = 8, y2 = -4;
+    void whenPointsMinus4And6And8AndMinus4Then15Dot62() {
+        Point point1 = new Point(-4, 6);
+        Point point2 = new Point(8, -4);
         double expected = 15.62;
-        double output = Point.distance(x1, y1, x2, y2);
-        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+        double result = point1.distance(point2);
+        assertThat(result).isEqualTo(expected, withPrecision(0.01));
     }
 }
